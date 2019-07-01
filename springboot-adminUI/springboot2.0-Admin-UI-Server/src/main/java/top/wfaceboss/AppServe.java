e646a4651840aa84bea020f7e7531f89e963e3d3
FelixBin <1948728748@qq.com>
2019-07-01 16:05:00 +0800

    集成adminui

--- springboot-adminUI/springboot2.0-Admin-UI-Server/src/main/java/top/wfaceboss/AppServe.java
+++ springboot-adminUI/springboot2.0-Admin-UI-Server/src/main/java/top/wfaceboss/AppServe.java
@@ -3,14 +3,14 @@ package top.wfaceboss;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * 启动类<br>
- * @author Administrator
- */
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+
+@EnableAdminServer
 @SpringBootApplication
-public class App {
+public class AppServe {
+
 	public static void main(String[] args) {
 		// 程序入口，启动SpringBoot项目 项目创建内置tomcat服务器 使用tomcat加载springmvc 注解启动类
-		SpringApplication.run(App.class, args);
+		SpringApplication.run(AppServe.class, args);
 	}
 }
