864227d1201a79b5c65f4e9695ecc3d2ceb2c301
FelixBin <1948728748@qq.com>
2019-07-01 17:08:18 +0800

    springboot2.0 集成jsp

---
+++ springboot2.0.JSP/src/main/java/top/wfaceboss/App.java
@@ -0,0 +1,17 @@
+package top.wfaceboss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类<br>
+ * @author Administrator
+ *@SpringBootApplication=@EnableAutoConfiguration+@ComponentScan
+ */
+@SpringBootApplication
+public class App {
+	public static void main(String[] args) {
+		// 程序入口，启动SpringBoot项目 项目创建内置tomcat服务器 使用tomcat加载springmvc 注解启动类
+		SpringApplication.run(App.class, args);
+	}
+}
---
+++ springboot2.0.JSP/src/main/webapp/WEB-INF/jsp/index.jsp
@@ -0,0 +1,10 @@
+<html>
+
+<head>
+<title>springboot2.0+jsp</title>
+</head>
+<body>
+	<h1 style="color: red">springboot2.0  +  jsp succeed</h1>
+</body>
+
+</html>
