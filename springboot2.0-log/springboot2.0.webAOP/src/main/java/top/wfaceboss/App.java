c52555f722864e18ccb5140b31c134de43a5c8ab
FelixBin <1948728748@qq.com>
2019-07-02 09:37:56 +0800

    springboot2.0使用aop技术统一处理web请求日志

---
+++ springboot2.0-log/springboot2.0.webAOP/src/main/java/top/wfaceboss/App.java
@@ -0,0 +1,17 @@
+package top.wfaceboss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类<br>
+ * 
+ * @author Administrator
+ *
+ */
+@SpringBootApplication
+public class App {
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+}
